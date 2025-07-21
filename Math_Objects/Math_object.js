3a56d5b06408d0a82306a688bf943aa4a27ddc2b
Ramachandran <svel936@gmail.com>
2025-07-21 07:56:17 +0530

    commit

---
+++ Math_Objects/Math_object.js
@@ -0,0 +1,39 @@
+// math.sqtr 
+// Math object in JavaScript
+// const mathObject = {
+//     PI: Math.PI,
+//     E: Math.E,
+//     sqrt: function(x) {
+//         return Math.sqrt(x);
+//     },
+//     pow: function(base, exponent) {
+//         return Math.pow(base, exponent);
+//     },
+//     random: function() {
+//         return Math.random().toFixed(2);
+//     },
+//     round: function(x) {
+//         return Math.round(x);
+//     },
+//     ceil: function(x) {
+//         return Math.ceil(x);
+//     },
+//     floor: function(x) {
+//         return Math.floor(x);
+//     }
+// };
+
+// // Example usage
+// console.log("Value of PI:", mathObject.PI);
+// console.log("Square root of 16:", mathObject.sqrt(16));
+// console.log("2 raised to the power of 3:", mathObject.pow(2, 3));
+// console.log("Random number:", mathObject.random().toString(2));     
+// console.log("Round 4.7:", mathObject.round(4.7));
+// console.log("Ceil 4.1:", mathObject.ceil(4.1));
+// console.log("Floor 4.9:", mathObject.floor(4.9));
+
+
+// math.trunc   
+// Math object in JavaScript
+// console.log(Math.trunc(4.9)); // 4
+// console.log(Math.trunc(4.1)); // 4
