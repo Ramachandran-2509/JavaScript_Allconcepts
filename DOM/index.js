5ad445722426b0e4d874e3a429d23dd28c7ea6e6
Ramachandran <svel936@gmail.com>
2025-07-29 21:20:38 +0530

    commit

---
+++ DOM/index.js
@@ -0,0 +1,35 @@
+// DOM Techniques
+//  1. Select Elements
+// 2.Travesing Elements
+// 3. Manipulating Elements
+
+// 1.select elements
+
+// let heading=document.getElementsByTagName("h1")
+// console.log(heading)
+
+// let para=document.getElementsByClassName("p")
+// console.log(para)
+
+// let p=document.getElementById('check')
+// console.log(p)
+
+// let k=document.getElementsByName('test')
+// console.log(k)
+
+
+let res=document.querySelector('h1')
+console.log(res)
+
+let p=document.querySelectorAll('.para1')
+console.log(p)
+
+let para=document.querySelectorAll('#check')
+console.log(para)
+
+
+// create element
+
+let h1=document.createElement('h1')
+h1.innerHTML="Hello world"
+document.body.appendChild(h1)
